db887e2146f9b4530f0b04ef41e10e493caeaf94
Kathryn Killebrew <kkillebrew@azavea.com>
2015-12-29 16:28:21 -0500

    Bump version number

--- androidvalidatedforms/src/main/java/com/azavea/androidvalidatedforms/FormWithAppCompatActivity.java
+++ androidvalidatedforms/src/main/java/com/azavea/androidvalidatedforms/FormWithAppCompatActivity.java
@@ -7,7 +7,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager.LayoutParams;
