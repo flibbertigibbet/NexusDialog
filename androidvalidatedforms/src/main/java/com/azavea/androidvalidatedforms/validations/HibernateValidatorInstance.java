0d6c1e1ecfe07c16825fcc3d9a8a90f5608858a0
Kathryn Killebrew <kkillebrew@azavea.com>
2016-06-17 18:12:47 -0400

    Add custom validator message translations.
    Also bump support library versions.

--- androidvalidatedforms/src/main/java/com/azavea/androidvalidatedforms/validations/HibernateValidatorInstance.java
+++ androidvalidatedforms/src/main/java/com/azavea/androidvalidatedforms/validations/HibernateValidatorInstance.java
@@ -1,14 +1,27 @@
 package com.azavea.androidvalidatedforms.validations;
 
-import org.hibernate.validator.HibernateValidator;
+import android.app.Application;
+import android.util.Log;
 
+import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
+import org.hibernate.validator.resourceloading.PlatformResourceBundleLocator;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import javax.validation.Validation;
 import javax.validation.ValidationProviderResolver;
 import javax.validation.Validator;
 import javax.validation.spi.ValidationProvider;
+
+import dalvik.system.PathClassLoader;
 
 /**
  * Share a single Hibernate Validator instance across the app.
@@ -28,6 +41,12 @@ public class HibernateValidatorInstance {
     }
 
     private static Validator createValidator() {
+
+        // Define a resource bundle that will find the appropriate ValidationMessages_<locale>.properties
+        // file on the classpath.
+        PlatformResourceBundleLocator resourceBundleLocator =
+                new PlatformResourceBundleLocator(ResourceBundleMessageInterpolator.USER_VALIDATION_MESSAGES);
+
         return Validation
                 .byProvider(HibernateValidator.class)
                 .providerResolver(new ValidationProviderResolver() {
@@ -38,6 +57,7 @@ public class HibernateValidatorInstance {
                 })
                 .configure()
                 .ignoreXmlConfiguration()
+                .messageInterpolator(new ResourceBundleMessageInterpolator(resourceBundleLocator))
                 .buildValidatorFactory().getValidator();
     }
 }
